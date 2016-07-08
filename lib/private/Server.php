fe5d6fb3306ab0a0cc22e1bccded64726ac0d6d7
Vincent Petry <pvince81@owncloud.com>
2016-07-08 17:11:46 +0200

    Move BackendService to core as IStoragesBackendService
    Can be queried with \OC::$server->getStoragesBackendService()
    Implements the new interface IStoragesBackendService which also
    contains the former constants.

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -86,6 +86,7 @@ use OCP\IServerContainer;
 use OCP\Security\IContentSecurityPolicyManager;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use OC\Files\External\StoragesBackendService;
 
 /**
  * Class Server
@@ -662,6 +663,9 @@ class Server extends ServerContainer implements IServerContainer {
 		});
 		$this->registerService('ContentSecurityPolicyManager', function (Server $c) {
 			return new ContentSecurityPolicyManager();
+		});
+		$this->registerService('StoragesBackendService', function (Server $c) {
+			return new StoragesBackendService($c->query('AllConfig'));
 		});
 		$this->registerService('ShareManager', function(Server $c) {
 			$config = $c->getConfig();
@@ -1328,10 +1332,10 @@ class Server extends ServerContainer implements IServerContainer {
 	/**
 	 * Not a public API as of 8.2, wait for 9.0
 	 *
-	 * @return \OCA\Files_External\Service\BackendService
+	 * @return \OCP\Files\External\IStoragesBackendService
 	 */
 	public function getStoragesBackendService() {
-		return \OC_Mount_Config::$app->getContainer()->query('OCA\\Files_External\\Service\\BackendService');
+		return $this->query('StoragesBackendService');
 	}
 
 	/**
